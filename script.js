b4a459125a9fca966b2d18e91af1b52d3b94c73b
FIRST_Imad LAST_Kajdouh <2058092@talnet.nl>
2020-03-13 10:44:14 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,10 @@
+var mensen = 
+{
+    naam: "Zara",
+    score: 36,
+    begin_datum: "21 januari 2016",
+    vrienden: ["Mike", "Josef", "Sumail", "Lotte"],
+    gekwalificeerd: true
+};
+
+console.log(JSON.stringify(mensen));
